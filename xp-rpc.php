457555643a9e3a971aee88990cf85e53584f78aa
Mitch <mitchellvanw@gmail.com>
2018-02-16 00:21:22 +0100

    Add files via upload

---
+++ xp-rpc.php
@@ -0,0 +1,213 @@
+<?php
+/*
+XP-PHP
+
+XP-PHP is a class for making calls to eXperience Points' API using PHP.
+
+====================
+
+The MIT License (MIT)
+
+Copyright (c) 2018 Mitch (Coinvergence)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+====================
+
+// Initialize eXperience Points connection/object
+$xp = new XPd('username','password');
+
+// Optionally, you can specify a host and port.
+$xp = new XPd('username','password','host','port');
+// Defaults are:
+//	host = localhost
+//	port = 21892
+//	proto = http
+
+// If you wish to make an SSL connection you can set an optional CA certificate or leave blank
+// This will set the protocol to HTTPS and some CURL flags
+$xp->setSSL('/full/path/to/certificate.cert');
+
+// Make calls to XPd as methods for your object. Responses are returned as an array.
+// Examples:
+$xp->getinfo();
+$xp->getrawtransaction('8669d910e1388d6e86b3e4f18b533748ba9f48add814128d47d9c367124c76a7',1);
+$xp->getblock('edb97f362767d3353537cec37ba49a6fb0c8dcd37a47df220f0542c1a34ad8e3');
+
+// The full response (not usually needed) is stored in $this->response
+// while the raw JSON is stored in $this->raw_response
+
+// When a call fails for any reason, it will return FALSE and put the error message in $this->error
+// Example:
+echo $xp->error;
+
+// The HTTP status code can be found in $this->status and will either be a valid HTTP status code
+// or will be 0 if cURL was unable to connect.
+// Example:
+echo $xp->status;
+
+*/
+
+class XPd
+{
+    // Configuration options
+    private $username;
+    private $password;
+    private $proto;
+    private $host;
+    private $port;
+    private $url;
+    private $CACertificate;
+
+    // Information and debugging
+    public $status;
+    public $error;
+    public $raw_response;
+    public $response;
+
+    private $id = 0;
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @param string $host
+     * @param int $port
+     * @param string $proto
+     * @param string $url
+     */
+    public function __construct($username, $password, $host = 'localhost', $port = 21892, $url = null)
+    {
+        $this->username      = $username;
+        $this->password      = $password;
+        $this->host          = $host;
+        $this->port          = $port;
+        $this->url           = $url;
+
+        // Set some defaults
+        $this->proto         = 'http';
+        $this->CACertificate = null;
+    }
+
+    /**
+     * @param string|null $certificate
+     */
+    public function setSSL($certificate = null)
+    {
+        $this->proto         = 'https'; // force HTTPS
+        $this->CACertificate = $certificate;
+    }
+
+    public function __call($method, $params)
+    {
+        $this->status       = null;
+        $this->error        = null;
+        $this->raw_response = null;
+        $this->response     = null;
+
+        // If no parameters are passed, this will be an empty array
+        $params = array_values($params);
+
+        // The ID should be unique for each call
+        $this->id++;
+
+        // Build the request, it's ok that params might have any empty array
+        $request = json_encode(array(
+            'method' => $method,
+            'params' => $params,
+            'id'     => $this->id
+        ));
+
+        // Build the cURL session
+        $curl    = curl_init("{$this->proto}://{$this->host}:{$this->port}/{$this->url}");
+        $options = array(
+            CURLOPT_HTTPAUTH       => CURLAUTH_BASIC,
+            CURLOPT_USERPWD        => $this->username . ':' . $this->password,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_HTTPHEADER     => array('Content-type: application/json'),
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => $request
+        );
+
+        // This prevents users from getting the following warning when open_basedir is set:
+        // Warning: curl_setopt() [function.curl-setopt]:
+        //   CURLOPT_FOLLOWLOCATION cannot be activated when in safe_mode or an open_basedir is set
+        if (ini_get('open_basedir')) {
+            unset($options[CURLOPT_FOLLOWLOCATION]);
+        }
+
+        if ($this->proto == 'https') {
+            // If the CA Certificate was specified we change CURL to look for it
+            if (!empty($this->CACertificate)) {
+                $options[CURLOPT_CAINFO] = $this->CACertificate;
+                $options[CURLOPT_CAPATH] = DIRNAME($this->CACertificate);
+            } else {
+                // If not we need to assume the SSL cannot be verified
+                // so we set this flag to FALSE to allow the connection
+                $options[CURLOPT_SSL_VERIFYPEER] = false;
+            }
+        }
+
+        curl_setopt_array($curl, $options);
+
+        // Execute the request and decode to an array
+        $this->raw_response = curl_exec($curl);
+        $this->response     = json_decode($this->raw_response, true);
+
+        // If the status is not 200, something is wrong
+        $this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        // If there was no error, this will be an empty string
+        $curl_error = curl_error($curl);
+
+        curl_close($curl);
+
+        if (!empty($curl_error)) {
+            $this->error = $curl_error;
+        }
+
+        if ($this->response['error']) {
+            // If XPd returned an error, put that in $this->error
+            $this->error = $this->response['error']['message'];
+        } elseif ($this->status != 200) {
+            // If XPd didn't return a nice error message, we need to make our own
+            switch ($this->status) {
+                case 400:
+                    $this->error = 'HTTP_BAD_REQUEST';
+                    break;
+                case 401:
+                    $this->error = 'HTTP_UNAUTHORIZED';
+                    break;
+                case 403:
+                    $this->error = 'HTTP_FORBIDDEN';
+                    break;
+                case 404:
+                    $this->error = 'HTTP_NOT_FOUND';
+                    break;
+            }
+        }
+
+        if ($this->error) {
+            return false;
+        }
+
+        return $this->response['result'];
+    }
+}
